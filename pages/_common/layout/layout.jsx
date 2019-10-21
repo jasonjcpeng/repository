08d90daaf60e9a3b08bb15d4b0ffc45117809cf8
pengjingcheng <pengjingcheng@huya.com>
2019-10-21 17:56:14 +0800

    chore:减少一个节点

--- pages/_common/layout/layout.jsx
+++ pages/_common/layout/layout.jsx
@@ -1,10 +1,10 @@
 import Head from './header';
 
 export default ({ children, title = 'This is the default title' }) => (
-  <div>
+  <React.Fragment>
     <Head title={title}></Head>
     {children}
     <footer>
     </footer>
-  </div>
+  </React.Fragment>
 )
